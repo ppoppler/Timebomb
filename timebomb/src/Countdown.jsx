0e4be91e6509087ed3bc1e070682c76448b20909
Philip Poppler <philpoppler@gmail.com>
2019-03-30 03:40:28 -0700

    Add Countdown Components

---
+++ timebomb/src/Countdown.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+
+/**
+ * Returns a single countdown object. The time will be initially set but using state, the countdown time will change
+ */
+export default class Countdown extends Component {
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
---
+++ timebomb/src/CountdownGrid.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Grid,Row,Col} from 'react-bootstrap';
+import Countdown from './Countdown';
+
+/**
+ * Contains a grid of all the countdown objects
+ */
+export default class CountdownGrid extends Component{
+    render(){
+        return(
+
+        );
+    }
+}
