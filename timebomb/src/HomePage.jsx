12a7a2536fe6efd10d7371eca5fbf45616f9da45
Philip Poppler <philpoppler@gmail.com>
2019-03-30 13:56:58 -0700

    Add Settings Icon

--- timebomb/src/HomePage.jsx
+++ timebomb/src/HomePage.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import { Navbar, Container } from "react-bootstrap";
 import clock from "./img/clock_icon.png";
+import setting from "./img/settings_icon.png";
 
 export default class HomePage extends Component {
   render() {
     return (
       <Navbar transparent fixed="top" expand="lg">
         <Navbar.Collapse className="justify-content-end">
-          <a href="login">
+          <a href="add_clock">
             <img className="small-icon" src={clock} />
+          </a>
+          <a href="settings">
+            <img className="small-icon" src={setting} />
           </a>
         </Navbar.Collapse>
       </Navbar>
