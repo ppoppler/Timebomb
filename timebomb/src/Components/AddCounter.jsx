fba59f92e350da8c1637c4d5a56d3ea63aff884a
Philip Poppler <philpoppler@gmail.com>
2019-03-31 08:54:37 -0700

    Fix Button

--- timebomb/src/Components/AddCounter.jsx
+++ timebomb/src/Components/AddCounter.jsx
@@ -2,17 +2,13 @@ import React, { Component } from "react";
 import { Form, Button } from "react-bootstrap";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import '../App.css';
+import "../App.css";
 /**
  * Prompts the user to add another counter
  */
 export default class AddCounter extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      endDate: new Date()
-      
-    };
     this.handleChange = this.handleChange.bind(this);
   }
 
@@ -39,34 +35,38 @@ export default class AddCounter extends Component {
 
   render() {
     return (
-      <div><h1>Create Countdown</h1>
-      <div className="addCounterForm">
-        <Form onSubmit={this.add.bind(this)}>
-          <Form.Group>
-            <Form.Control
-              id="eventNameForm"
-              type="text"
-              value = {this.state.title}
-              onChange={this.handleText.bind(this)}
-              placeholder="Event name"
+      <div>
+        <h1>Create Countdown</h1>
+        <div className="addCounterForm">
+          <Form onSubmit={this.add.bind(this)}>
+            <Form.Group>
+              <Form.Control
+                id="eventNameForm"
+                type="text"
+                value={this.state.title}
+                onChange={this.handleText.bind(this)}
+                placeholder="Event name"
+              />
+            </Form.Group>
+
+            <DatePicker
+              id="dateForm"
+              placeholderText="Click to select a date"
+              selected={this.state.endDate}
+              onChange={this.handleChange}
+              timeInputLabel="Time:"
+              showTimeInput
+              timeIntervals={30}
+              isClearable={true}
+              withPortal
+              timeCaption="Time"
+              dateFormat="MMMM d, yyyy h:mm aa"
             />
-          </Form.Group>
-        </Form>
-        <DatePicker
-          id="dateForm"
-          placeholderText="Click to select a date"
-          selected={this.state.endDate}
-          onChange={this.handleChange}
-          timeInputLabel="Time:"
-          showTimeInput
-          timeIntervals={30}
-          isClearable={true}
-          withPortal
-          timeCaption="Time"
-          dateFormat="MMMM d, yyyy h:mm aa"
-        />
-        <Button id="formButton" type="submit" variant="secondary">Create Countdown</Button>
-      </div>
+            <Button id="formButton" type="submit" variant="secondary">
+              Create Countdown
+            </Button>
+          </Form>
+        </div>
       </div>
     );
   }
